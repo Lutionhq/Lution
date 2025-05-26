06b0a1abd0fc5ad2ac94e406c0c5aa647e639828
the chip <roroxji.jk.k5d678@bumpmail.io>
2025-05-26 21:39:31 +0700

    omg i forgo to commit

--- src/sostrapter/app.py
+++ src/sostrapter/app.py
@@ -2,12 +2,41 @@
 import streamlit as st 
 import os 
 import json
+from pathlib import Path
 
 aboutmd = open(os.path.join(os.path.dirname(__file__), 'markdown/about.md')).read()
 
+file_path = os.path.expanduser("~/.var/app/org.vinegarhq.Sober/config/sober/config.json")
+
+try:
+    with open(file_path, 'r') as f:
+        data = json.load(f)
+    sober_config = json.dumps(data)
+except FileNotFoundError:
+    st.sidebar.error(f"Error: The file '{file_path}' was not found.")
+    print(f"Error: The file '{file_path}' was not found.")
+except json.JSONDecodeError:
+    st.sidebar.error(f"Error: Could not decode JSON from the file '{file_path}'.")
+    print(f"Error: Could not decode JSON from the file '{file_path}'.")
+except Exception as e:
+    st.sidebar.error(f"An unexpected error occurred: {e}")
+    print(f"An unexpected error occurred: {e}")
+
+def update_fflag(flag_name, flag_value):
+    try:
+        if "fflags" not in sober_config or not isinstance(sober_config["fflags"], dict):
+            sober_config["fflags"] = {}
+        sober_config["fflags"][flag_name] = flag_value
+        with open(file_path, "w") as f:
+            json.dump(sober_config, f, indent=4)
+        st.success(f"fflags['{flag_name}'] set to {flag_value}")
+    except Exception as e:
+        st.error(f"Failed to update fflags: {e}")
+
+
 def success():
     """Display a success message."""
-    st.success('', icon="✅")
+    st.susccess('', icon="✅")
 
 def apply_changes(fpslimit, lightingtech):
     """Apply changes based on user input."""
--- src/sostrapter/markdown/about.md
+++ src/sostrapter/markdown/about.md
@@ -2,6 +2,6 @@
 
 This project made by Chip, using Streamlit, made possible with [Sober](https://sober.vinegarhq.org/).
     
-You can find the source code on [GitHub](https://github.com/triisdang/Sostrapter),**Consider give us a star! ⭐**
+You can find the source code on [GitHub](https://github.com/triisdang/Sostrapter), **Consider give us a star! ⭐**
 
 :rainbow[**Thank you for using Sostrapter! ❤️**]
