367af165c7c0f2f40585ef4bb919d01403336beb
the chip <roroxji.jk.k5d678@bumpmail.io>
2025-05-26 19:27:19 +0700

    basic tabs i will add into this project

--- src/sostrapter/app.py
+++ src/sostrapter/app.py
@@ -3,6 +3,26 @@ import streamlit as st
 # app.
 def success():
     """Display a success message."""
-    st.success('This is a success message!', icon="✅")
+    st.success('', icon="✅")
     
-st.button('Show Success Message', on_click=success)
+modstab , apprtab, about = st.tabs(["Mods", "Apppearance","About"])
+
+with modstab:
+    st.header("Mods")
+    st.write("This is the Mods tab. You can add your mods here.")
+    st.button("Add Mod", on_click=success)
+
+with apprtab:
+    st.header("Appearance")
+    st.write("This is the Appearance tab. You can customize the appearance here.")
+    st.button("Change Appearance", on_click=success)
+with about:
+    st.header("About")
+    '''
+    This project made by Chip, using Streamlit, made possible with [Sober](https://sober.vinegarhq.org/).
+    
+    You can find the source code on [GitHub](https://github.com/triisdang/Sostrapter)
+
+    Thank you for using Sostrapter!❤️
+    '''
+
