e97a6052dd613b4017cfebe29f1f36eabc1b0bf9
the chip <roroxji.jk.k5d678@bumpmail.io>
2025-05-26 19:58:12 +0700

    ts not do anything yet, js layout

--- src/sostrapter/app.py
+++ src/sostrapter/app.py
@@ -1,28 +1,27 @@
+# src/sostrapter/app.py
 import streamlit as st 
+import os 
 
-# app.
+aboutmd = open(os.path.join(os.path.dirname(__file__), 'markdown/about.md')).read()
+
 def success():
     """Display a success message."""
     st.success('', icon="✅")
     
-modstab , apprtab, about = st.tabs(["Mods", "Apppearance","About"])
+modstab , apprtab, about,ff = st.tabs(["Mods", "Apppearance","About", "Fast Flags"])
 
 with modstab:
     st.header("Mods")
     st.write("This is the Mods tab. You can add your mods here.")
     st.button("Add Mod", on_click=success)
+with ff:
+    st.header("Fast Flags")
+    st.text_input("FPS Limit","60")
 
 with apprtab:
     st.header("Appearance")
     st.write("This is the Appearance tab. You can customize the appearance here.")
     st.button("Change Appearance", on_click=success)
 with about:
-    st.header("About")
-    '''
-    This project made by Chip, using Streamlit, made possible with [Sober](https://sober.vinegarhq.org/).
-    
-    You can find the source code on [GitHub](https://github.com/triisdang/Sostrapter)
-
-    Thank you for using Sostrapter!❤️
-    '''
+    st.markdown(aboutmd)
 
