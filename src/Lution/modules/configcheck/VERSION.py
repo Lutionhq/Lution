5b27e28391bf38cb75df27d5d822b823f0826576
the chip <roroxji.jk.k5d678@bumpmail.io>
2025-06-22 09:43:29 -0400

    track the new thing yeah

--- src/Lution/modules/configcheck/VERSION.py
+++ src/Lution/modules/configcheck/VERSION.py
@@ -1,15 +1,16 @@
-# AUTO GENARATED, DO NOT TRY TO MODIFY
+# AUTO GENERATED, DO NOT MODIFY
 # sigmas
-GIT_COMMITT = "06da0ebead4c15fc91f09efe9d4307edeeb5076d"
-COMMIT_DATE = "2025-06-22 09:28:22 -0400"
+GIT_COMMITT = "1ede314c0a28e0d9f68f6943b791270f784d1041"
+COMMIT_DATE = "2025-06-22 09:29:11 -0400"
 
 from github import Github
 
 g = Github()
 repo = g.get_repo("lutionhq/lution")
-ltcommit = repo.get_commits()[1]
-if ltcommit.sha == GIT_COMMITT:
-    GIT_COMMIT = f"{GIT_COMMITT} (outdated)"
+ltcommit = repo.get_commits()[0]
+
+if ltcommit.sha.startswith(GIT_COMMITT):
+    GIT_COMMIT = f"{GIT_COMMITT}"
 else:
-    GIT_COMMIT = GIT_COMMITT
+    GIT_COMMIT = f"{GIT_COMMITT} (outdated)"
 
