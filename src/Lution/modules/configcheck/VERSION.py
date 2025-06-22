06da0ebead4c15fc91f09efe9d4307edeeb5076d
the chip <roroxji.jk.k5d678@bumpmail.io>
2025-06-22 09:28:22 -0400

    precommit

--- src/Lution/modules/configcheck/VERSION.py
+++ src/Lution/modules/configcheck/VERSION.py
@@ -1,15 +1,15 @@
 # AUTO GENARATED, DO NOT TRY TO MODIFY
 # sigmas
-GIT_COMMITT = "8990eee79880adb3ecc0701affc5616ed6e36740"
-COMMIT_DATE = "2025-06-22 09:13:22 -0400"
+GIT_COMMITT = "7d534af99ac353444a8754adeed7183143b2df68"
+COMMIT_DATE = "2025-06-22 09:14:11 -0400"
 
 from github import Github
 
 g = Github()
-repo = "lutionhq/lution"
-
-ltcommit = g.get_repo(repo)
-if not ltcommit == GIT_COMMITT :
+repo = g.get_repo("lutionhq/lution")
+ltcommit = repo.get_commits()[1]
+if ltcommit.sha == GIT_COMMITT:
     GIT_COMMIT = f"{GIT_COMMITT} (outdated)"
 else:
     GIT_COMMIT = GIT_COMMITT
+
