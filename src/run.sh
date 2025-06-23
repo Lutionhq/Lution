d16f61b0c00e95517c7ed7e147f92f915334eb32
evil devil chip <roroxji.jk.k5d678@bumpmail.io>
2025-06-24 05:40:36 +0700

    cd out and .venv thing

--- src/run.sh
+++ src/run.sh
@@ -8,6 +8,7 @@ cat << "EOF"
  \ \_____\  \ \_____\    \ \_\  \ \_\  \ \_____\  \ \_\\"\_\
   \/_____/   \/_____/     \/_/   \/_/   \/_____/   \/_/ \/_/
 EOF
+
 if grep -qi 'ubuntu' /etc/os-release; then
     if ! dpkg -s python3-venv >/dev/null 2>&1; then
         echo "Installing python3-venv..."
@@ -15,14 +16,13 @@ if grep -qi 'ubuntu' /etc/os-release; then
     else
         echo "python3-venv is already installed."
     fi
-else
-    echo ""
 fi
 
-python3 -m venv venv
-source venv/bin/activate
+cd ..
+python3 -m venv ".venv"
+source .venv/bin/activate
 
-cd "Lution" || exit 1
+cd "src/Lution" || exit 1
 pip install -r requirements.txt
 
 clear
@@ -37,7 +37,6 @@ EOF
 echo "It should open a tab in your browser, if not click on the link"
 echo "Also do you love my ASCII text? :3"
 
-streamlit run main.py || python3 -m streamlit run main.py
-
+streamlit run main.py
 
 echo "i got destroyed 😭"
