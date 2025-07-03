ad623df404ab779bb4fe98e77962980b6d11da06
cihp <itsjustchipoverhere@proton.me>
2025-07-03 12:47:49 +0700

    minor fixes

--- src/Lution/modules/muti_instance/SoberManager.py
+++ src/Lution/modules/muti_instance/SoberManager.py
@@ -3,9 +3,10 @@ import subprocess
 from pathlib import Path
 from modules.utils.logging import *
 
-
-
 # Original from DiM, forked by chip
+class InstanceAlreadyExist(Exception):
+    pass
+
 class SoberManager():
     def __init__(self):
         self.name = None
@@ -17,39 +18,39 @@ class SoberManager():
         """Add an Instance."""
         appID = "org.vinegarhq.Sober"
         flatpakpath = Path("/var/lib/flatpak/app")
-        
+
         orig = flatpakpath / appID
         new_dir = flatpakpath / name
 
-        if new_dir.exists() :
-            raise InstanceAlreadyExist
+        if new_dir.exists():
+            raise InstanceAlreadyExist()
 
         if not orig.exists():
             warn("Sober not installed, installing..", "MUTI INSTANCE")
-            subprocess.run(["flatpak", "install", "--user", cls.appID, "-y"], check=True)
+            subprocess.run(["flatpak", "install", "--user", appID, "-y"], check=True)
 
         shutil.copytree(orig, new_dir)
 
         # Mark this folder as our created instance
         (new_dir / ".sober_instance").touch()
         
-        cls.UpdateMetaData(name)
-        print(f"Account '{cls.name}' created")
+        instance = cls()
+        instance.UpdateMetaData(name)
+        print(f"Account '{name}' created")
     
-
     def UpdateMetaData(self, name):
-        stable_dir = self.path / name / "x86_64" / "stable"
+        stable_dir = self.flatpakpath / name / "x86_64" / "stable"
         if not stable_dir.exists():
-            warn(f"Stable directory {stable_dir} does not exist yet, skipping metadata patch","MUTI INSTANCE")
+            warn(f"Stable directory {stable_dir} does not exist yet, skipping metadata patch", "MUTI INSTANCE")
             return
 
         for entry in stable_dir.iterdir():
             if entry.is_dir() and entry.name != "active":
                 possible_metadata = entry / "metadata"
                 if possible_metadata.exists():
-                    info(f"Patching metadata: {possible_metadata}","MUTI INSTANCE")
+                    info(f"Patching metadata: {possible_metadata}", "MUTI INSTANCE")
                     text = possible_metadata.read_text()
-                    text = text.replace(f"name={account_name}") # remove appid
+                    text = text.replace("name=org.vinegarhq.Sober", f"name={name}")
                     possible_metadata.write_text(text)
                     return
 
