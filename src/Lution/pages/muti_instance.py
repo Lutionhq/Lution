37553229e720c580d0933f48a62204ef30f0eaf1
cihp <itsjustchipoverhere@proton.me>
2025-07-03 15:00:17 +0700

    add list

--- src/Lution/pages/muti_instance.py
+++ src/Lution/pages/muti_instance.py
@@ -10,10 +10,26 @@ def ask():
     st.write("With Sober instance, you can have many accounts on each instance and launch them all at once!")
     InstanceName = st.text_input(label="Instance name", placeholder="e.g: Profile1, MyNewAccount")
     if st.button("Add"):
-        SoberManager.add_instance(InstanceName)
+        with st.spinner("Please wait..."):
+            SoberManager.add_instance(InstanceName)
         st.success("Success! Don't spam it or its will trash your app folder....")
        
         
 st.write("Run sudo chmod -R 777 /var/lib/flatpak/app/ before create an instance")
 if st.button("Add new Instance") :
     ask()
+
+if st.button("get list sigma"):
+    st.write(SoberManager.list_instance())
+
+expander = st.expander("Instance", expanded=True)
+with expander :
+    accountlist = SoberManager.list_instance()
+    for i in accountlist :
+        colleft, colmid, colright = st.columns(3)
+        with colleft:
+            st.write(f"Instance {i}")
+        with colmid:
+            st.button("Delete",key=f"del{i}", use_container_width=True)
+        with colright:
+            st.button("Run",key=i, use_container_width=True)
