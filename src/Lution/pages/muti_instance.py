a74a29b48108aaa86f90bcbf8dd80e6a309bfa73
cihp <itsjustchipoverhere@proton.me>
2025-07-03 14:08:08 +0700

    mroe stuff

--- src/Lution/pages/muti_instance.py
+++ src/Lution/pages/muti_instance.py
@@ -4,14 +4,16 @@ from modules.utils.sidebar import InitSidebar
 
 InitSidebar()
 
-sm = SoberManager
 
 @st.dialog("Add A new Sober Instance")
 def ask():
     st.write("With Sober instance, you can have many accounts on each instance and launch them all at once!")
     InstanceName = st.text_input(label="Instance name", placeholder="e.g: Profile1, MyNewAccount")
     if st.button("Add"):
-        st.rerun()
-
+        SoberManager.add_instance(InstanceName)
+        st.success("Success! Don't spam it or its will trash your app folder....")
+       
+        
+st.write("Run sudo chmod -R 777 /var/lib/flatpak/app/ before create an instance")
 if st.button("Add new Instance") :
     ask()
